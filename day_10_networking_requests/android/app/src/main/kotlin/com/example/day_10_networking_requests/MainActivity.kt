1a7020cfa9b241601d087584ee96bf76981a27bd
Ruhit Arman <armanruhit09@gmail.com>
2021-03-26 12:00:45 +0600

    Signed-off-by: Ruhit Arman <armanruhit09@gmail.com>

---
+++ day_10_networking_requests/android/app/src/main/kotlin/com/example/day_10_networking_requests/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.day_10_networking_requests
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ day_10_networking_requests/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:day_10_networking_requests/data_service.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _dataService = DataService();
+  String _response;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Builder(builder: (_){
+            if(_response!=null){
+              return Text(_response);
+            }
+            else{
+              return ElevatedButton(
+                child: Text('Make Request'),
+                onPressed: _makeRequest,
+              );
+            }
+          }
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _makeRequest() async{
+    final response = await _dataService.makeRequestToApi();
+    setState(() {
+      _response = response;
+    });
+  }
+}
